c083aec956ef885b0f81a7b0e44260725c4e898f
w4yn0t <maksim.star4enko@gmail.com>
2021-11-16 11:35:05 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date.toString());
+    }
+}
