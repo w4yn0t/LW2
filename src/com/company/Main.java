36c6466f67d0485d0f9ac661fde06423c84995ba
w4yn0t <maksim.star4enko@gmail.com>
2021-11-19 14:04:00 +0200

    19.11.2021

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ import java.util.Date;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("DATE:");
         Date date = new Date();
         System.out.println(date.toString());
     }
